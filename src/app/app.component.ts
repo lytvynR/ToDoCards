3810b42748eb1ddf5f1b6a8ef331aa1f4c44576b
LytvynR <Ryslan.2240106@gmail.com>
2017-10-10 18:10:26 +0300

    first todo page

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+
+export class Item {
+  cardTitle: string;
+  constructor(cardTitle: string){
+    this.cardTitle = cardTitle;
+  }
+
+}
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+
+
+export class AppComponent {
+  cardTitle: string;
+
+  items: Item[] = [];
+
+
+  addNewToDo(text: string): void {
+    if(text==null || text==undefined || text.trim()=="")
+      return;
+    this.items.push(new Item(text));
+    this.clear();
+  }
+  clear(){
+      this.cardTitle=""
+  }
+  deleteTodo(index) {
+    this.items.splice(index, 1);
+  }
+ 
+}
